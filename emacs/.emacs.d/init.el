c4d4fe593edb7757d2e197fc709f78196925d530
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-05-31 01:06:17 +0200

    Adding simple innstallation script for now, and a start of some emacs configuration

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,28 @@
+; Marmalade repository for package goodness
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;
+; TODO: When first installing:
+;
+; M-x package-refresh-contents
+;
+
+
+; Packages I use
+(defvar my-packages '(starter-kit
+		      starter-kit-lisp
+		      starter-kit-bindings
+		      starter-kit-eshell
+		      clojure-mode
+		      clojure-test-mode
+		      nrepl))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+; Theme and visual stuff
+(load-theme 'solarized-dark t)
+(set-face-attribute 'default nil :font "Inconsolata-16")
