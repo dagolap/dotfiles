52a2c7390ff643cfc9d4f561f412c7018f7dcf7f
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-06-04 19:56:20 +0200

    Fixing backup movement commands

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-dir = `pwd`
+cur_dir=`pwd`
 
 # Emacs
-[[ -d $HOME/.emacs.d ]] && mv $HOME/.emacs.d-bkp
-[[ -f $HOME/.emacs ]] && mv $HOME/.emacs-bkp
-ln -s `pwd`"/emacs/.emacs.d/" ~/.emacs.d
+[[ -d $HOME/.emacs.d ]] && mv $HOME/.emacs.d $HOME/.emacs.d-bkp
+[[ -f $HOME/.emacs ]] && mv $HOME/.emacs $HOME/.emacs-bkp
+ln -s $cur_dir/emacs/.emacs.d/ ~/.emacs.d
